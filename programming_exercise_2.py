ccff16bdb65c39b1180ced2ba9e1037c3b000594
Shinpai <juutilainen.harri@gmail.com>
2018-01-31 18:24:37 +0200

    pso test valmis

--- programming_exercise_2.py
+++ programming_exercise_2.py
@@ -22,32 +22,29 @@ class Particle:
                          rng.uniform(Y_LOWER, Y_UPPER)]
         self.velocity = [rng.uniform(0, MAX_VELOC) for i in range(DIMENSION)]
 
-        self.current_value = None
-        self.pbest = None
+        self.fitness = 0.0
+        self.pbest = 0.0
 
     def evaluate(self):
-        ''' Function to optimize (or minimize) '''
+        ''' Function to optimize (minimize) '''
         x = self.position[0]
         y = self.position[1]
-        self.current_value = (math.cos(x) * math.cos(y) - (x / (y ** 2 + 1)))
+        self.fitness = (math.cos(x) * math.cos(y) - (x / (y ** 2 + 1)))
 
     def compare_pbest(self):
-        ''' marks down the personal best value of a particle '''
-        if self.pbest is None:
-            self.pbest = self.current_value
-        if self.current_value < self.pbest:
-            self.pbest = self.current_value
+        ''' marks down the personal best value of a particle '''        
+        if self.fitness < self.pbest:
+            self.pbest = self.fitness
 
     def update_velocity(self, gbest):
         ''' randomly updates the velocity of a particle '''
         for i in range(DIMENSION):
             self.velocity[i] = self.velocity[i]
-            + ACC_CONST * rng.random() * (self.pbest - self.current_value)
-            + ACC_CONST * rng.random() * (gbest - self.current_value)
+            + ACC_CONST * rng.random() * (self.pbest - self.position[i])
+            + ACC_CONST * rng.random() * (gbest - self.position[i])
 
     def move(self):
-        for i in range(DIMENSION):
-            self.position[i] += self.velocity[i]
+        self.position = self.position + self.velocity
 
     def print_par(self):
         ''' Print info of a particle '''
@@ -56,14 +53,15 @@ class Particle:
               ',' + str(round(self.position[1], 3)) +
               '\n** Velocity x = ' + str(round(self.velocity[0], 3)) +
               '\n** Velocity y = ' + str(round(self.velocity[1], 3)) +
-              '\n** Current value = ' + str(round(self.current_value, 3)))
+              '\n** Current value = ' + str(round(self.fitness, 3)))
 
 
 class Swarm:
     ''' Set of Particles '''
     def __init__(self):
         self.particles = []
-        self.gbest = None
+        self.gbest = 0.0
+        self.gbest_pos = 0.0
 
     def initialize_swarm(self):
         ''' Initializes a swarm with size '''
@@ -71,35 +69,49 @@ class Swarm:
             self.particles.append(Particle(i))
 
     def compare_gbest(self, particle):
-        '''  '''
-        if self.gbest is None:
-            self.gbest = particle.current_value
-        if particle.current_value < self.gbest:
-            self.gbest = particle.current_value
+        ''' Compare value to best of swarm '''
+        if particle.fitness < self.gbest:
+            self.gbest = particle.fitness
+            self.gbest_pos = particle.position
 
-    def print_gen(self):
+    def compare_lbest(self, particle):
+        ''' Compare value to neighboring particles '''
+        if particle.index > 2:
+            n1 = self.particles[particle.index - 1]
+            n2 = self.particles[particle.index + 1]
+            if self.lbest is None:
+                self.lbest = particle.fitness
+            if particle.fitness < self.lbest:
+                self.lbest = particle.fitness
+
+    def print_gen(self, index):
         ''' prints out the necessary info for a generation '''
-        print('Swarm best: ' + str(round(min(i.pbest for i in self.particles), 3)))
+        print('\nRESULTS\n', '-'*7)
+        print('gbest fitness   : ', str(round(self.gbest, 3)))
+        print('gbest params    : ', str(self.gbest_pos))
+        print('iterations      : ', index)
 
 
-def pso_gbest(ITERATIONS):
+def PSO(ITERATIONS, index, mode):
     swarm = Swarm()
     swarm.initialize_swarm()
-    ITERATION = 0
-    while(ITERATION < ITERATIONS):
-        for particle in swarm.particles:
-            # evaluate particle position with function
-            particle.evaluate()
-            # compare value to particles best local value
+    for particle in swarm.particles:
+        # evaluate particle position with function
+        particle.evaluate()
+        if mode == 'gbest':
+            # compare value to populations best value
             particle.compare_pbest()
-            # compare value to swarms best global value
-            swarm.compare_gbest(particle)
-            # update velocity
-            particle.update_velocity(swarm.gbest)
-            # update position
-            particle.move()
-        ITERATION += 1
-    return round(swarm.gbest, 3)
+        else:
+            # compare value to best neighboring particle
+            swarm.compare_lbest(particle)
+        # compare value to swarms best global value
+        swarm.compare_gbest(particle)
+        # update velocity
+        particle.update_velocity(swarm.gbest)
+        # update position
+        particle.move()
+    if index == 100 or index == ITERATIONS:
+        swarm.print_gen(index)
 
 
 def clear():
@@ -117,23 +129,10 @@ def clear():
 
 def main():
     clear()
-    # record 100 and 2000 iterations with local and global, compare results
-    ITERATIONS = 100
-    tests = []
-    for i in range(10):
-        tests.append(pso_gbest(ITERATIONS))
-    print('Test on ' + str(ITERATIONS) + ' iterations')
-    for k in tests:
-        print('Swarm best value = ' + str(k))
-    
     ITERATIONS = 2000
-    tests = []
-    for i in range(10):
-        tests.append(pso_gbest(ITERATIONS))
-    print('Test on ' + str(ITERATIONS) + ' iterations')
-    for k in tests:
-        print('Swarm best value = ' + str(k))
-
+    mode = 'gbest'
+    for i in range(ITERATIONS + 1):
+        PSO(ITERATIONS, i, mode)
 
 ##############################################################################
 
